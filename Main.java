24202a7e7a8a9f07dcf9f9ee2b33b61369cf33a8
s30985 <162111201+mroczino@users.noreply.github.com>
2024-04-15 00:02:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
 public static void main(String[] args) {
-User guest1 = new User("lsobota@gmail.com");
-User guest2 = new User("miki@gmail.com");
-User user1 = new User("Adam","Dzewiecki","drzewo@gmail.com","drzewo11");
-guest2.guestToUser("Miki","Galecki","24513");
+User guest1 = new User("s30985@gmail.com");
+User guest2 = new User("xdxd@gmail.com");
+User user1 = new User("Marian","Kruszwil","marian@gmail.com","maniek8989");
+guest2.guestToUser("Adam","kaliński","86845");
 
-Product p1 = new Product("Milk", 10, "100090392", 2);
-Product p2 = new Product("Water", 2, "2347586");
-Product p3 = new Product("Bar", 3, "347654738", 10);
+Product p1 = new Product("jellybeans", 11, "1034312", 2);
+Product p2 = new Product("rockstarenergy", 2, "276886");
+Product p3 = new Product("Snickers", 6, "34535634", 10);
 p2.changePrice(3);
 p3.changeNumOfPieces(15);
 
